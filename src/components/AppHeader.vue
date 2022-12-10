68fa6fd3fda9917ba7ed224a88d7ee88b0da0a8f
Ariel201711 <arielk216@gmail.com>
2022-12-10 22:03:47 +0200

    corrected english syntax errors at app header - contactsand BTC

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,11 +5,11 @@
         <RouterLink to="/" class="logo clean-link">
           <h3>misterBitcoin.com</h3>
         </RouterLink>
-        <RouterLink to="/contact" class="clean-link">Contacs</RouterLink>
+        <RouterLink to="/contact" class="clean-link">Contacts</RouterLink>
         <RouterLink to="/stats" class="clean-link">Statistics</RouterLink>
       </nav>
       <div class="loggedin-user"> 
-        <h4>Welcome {{loggedInUser.name}}, Your Balance is: BTC{{loggedInUser.balance}}</h4>
+        <h4>Welcome {{loggedInUser.name}}, Your Balance is: BTC {{loggedInUser.balance}}</h4>
       </div>
     </div>
   </header>
